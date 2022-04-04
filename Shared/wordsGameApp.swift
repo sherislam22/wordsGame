2ab5d01262b2ce25a0a2919b7df91ff88d1883bc
Sherislam <sherislam02@yandex.ru>
2022-04-04 20:42:42 +0600

    Initial Commit

---
+++ Shared/wordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordsGameApp.swift
+//  Shared
+//
+//  Created by sher on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct wordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
